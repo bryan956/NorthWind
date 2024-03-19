2d92584fb751332c93ff06f4681465e7dec1b239
estudiantes <estudiantes@CLOCC5PC4769>
2024-03-19 16:59:20 -0500

    Enums

--- NorthWind.Entities/POCOEntities/Order.cs
+++ NorthWind.Entities/POCOEntities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NorthWind.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace NorthWind.Entities.POCOEntities
 {
-    internal class Order
+    public class Order
     {
+        public int Id { get; set; }
+        public string CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipCountry { get; set; }
+        public string ShipPostalCode { get; set; }
+
+        public DiscountType DiscountType { get; set; }
+        public double Discount { get; set; }
+        public ShippingType ShippingType { get; set; }
     }
 }
--- NorthWind.Entities/POCOEntities/OrderDetail.cs
+++ NorthWind.Entities/POCOEntities/OrderDetail.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace NorthWind.Entities.POCOEntities
 {
-    internal class OrderDetail
+    public class OrderDetail
     {
+        public int OrderId { get; set; } 
+        public int ProductId { get; set; }
+        public Decimal UnitPrice { get; set; }
+        public short Quantity { get; set; }
+        public Order Order { get; set; }
+
     }
 }
