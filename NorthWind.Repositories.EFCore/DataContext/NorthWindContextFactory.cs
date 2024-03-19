29521691b51c059d8aabe65fb7d19e6757a93f49
estudiantes <estudiantes@CLOCC5PC4769>
2024-03-19 17:58:39 -0500

    DataContext

---
+++ NorthWind.Repositories.EFCore/DataContext/NorthWindContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Design.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.DataContext
+{
+    internal class NorthWindContextFactory : IDesignTimeDbContextFactory<NorthWindContext>
+    {
+        public NorthWindContext CreateDbContext(string[] args)
+        {
+            var OptionsBuilder = 
+                new DbContextOptionsBuilder<NorthWindContext>();
+            OptionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssgllocaldb;database=NorthWindDB");
+            return new NorthWindContext(OptionsBuilder.Options);
+
+        }
+    }
+}
