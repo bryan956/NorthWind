f8fa4f123f745cdd5a71d8d39b69a275b75e7bb6
estudiantes <estudiantes@CLOCC5PC4769>
2024-03-20 17:14:35 -0500

    repositorios

---
+++ NorthWind.Repositories.EFCore/Repositories/OrderDetailRepositories.cs
@@ -0,0 +1,21 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.POCOEntities;
+using NorthWind.Repositories.EFCore.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.Repositories
+{
+    public class OrderDetailRepositories : IOrderDetailRepository
+    {
+        readonly NorthWindContext Context;
+        public OrderDetailRepositories(NorthWindContext context) => Context = context;
+        public void create(OrderDetail orderDitail)
+        {
+            Context.Add(orderDitail);
+        }
+    }
+}
