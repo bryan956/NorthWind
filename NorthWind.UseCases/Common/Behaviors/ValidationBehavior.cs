4ce5f039c9758b60404b150d6a5a114de9178e19
estudiantes <estudiantes@CLOCC5PC4769>
2024-03-20 18:10:31 -0500

    Final caso de uso

---
+++ NorthWind.UseCases/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCases.Common.Behaviors
+{
+    public class ValidationBehavior<TRquest, TResponse> : IPipelineBehavior<TRquest, TResponse>
+        where TResponse : IRequest<TResponse>
+    {
+        readonly IEnumerable<IValidator<TRquest>> Validators;
+        public ValidationBehavior(IEnumerable<IValidator<TRquest>> validators) => 
+            Validators = validators;
+
+        public Task<TResponse> Handle(TRquest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var Failures = Validators
+                .Select(v => v.Validate(request))
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null)
+                .ToList();
+            if(Failures.Any() )
+            {
+                throw new ValidationException(Failures);
+            }
+            return next();
+        }
+    }
+}
--- NorthWind.UseCases/CreateOrder/CreateOrderInteractor.cs
+++ NorthWind.UseCases/CreateOrder/CreateOrderInteractor.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using MediatR;
+using NorthWind.Entities.Exceptions;
+using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.POCOEntities;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NorthWind.UseCases.CreateOrder
 {
-    internal class CreateOrderInteractor
+    public class CreateOrderInteractor : IRequestHandler<CreateOrderInputPort, int>
     {
+        readonly IOrderRepository OrderRepository;
+        readonly IOrderDetailRepository OrderDetailRepository;
+        readonly IUnitOfWork UnitOfWork;
+
+        public CreateOrderInteractor(IOrderRepository orderRepository, IOrderDetailRepository orderDetailRepository, IUnitOfWork unitOfWork) => (OrderRepository, OrderDetailRepository, UnitOfWork) = (orderRepository, orderDetailRepository, unitOfWork);
+        public async Task<int> Handle(CreateOrderInputPort request, CancellationToken cancellationToken)
+        {
+            Order Order = new Order
+            {
+                CustomerId = request.CustomerId,
+                OrderDate = DateTime.Now,
+                ShipAddress = request.ShipAddress,
+                ShipCity = request.ShipCity,
+                ShipCountry = request.ShipCountry,
+                ShipPostalCode = request.ShipPostalCode,
+                ShippingType = Entities.Enums.ShippingType.Road,
+                DiscountType = Entities.Enums.DiscountType.Percentage,
+                Discount = 10
+            };
+
+            OrderRepository.Create(Order);
+            foreach(var Item in request.OrdeDetails)
+            {
+                OrderDetailRepository.create(
+                    new OrderDetail
+                    {
+                        Order = Order,
+                        ProductId = Item.ProducId,
+                        UnitPrice = Item.UniPrice,
+                        Quantity = Item.Quantity,
+                    });
+            }
+
+            try
+            {
+                await UnitOfWork.SaveChangesAsync();
+            }
+            catch(Exception ex)
+            {
+                throw new GeneralException("Erro al crear la orden", ex.Message);
+            }
+            return Order.Id;
+        }
     }
 }
