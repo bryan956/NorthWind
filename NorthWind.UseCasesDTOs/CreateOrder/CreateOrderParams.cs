a5506ce0bab5563478559d7c0f78c055859fd0fa
estudiantes <estudiantes@CLOCC5PC4769>
2024-03-20 17:48:49 -0500

    CreateOrder

---
+++ NorthWind.UseCasesDTOs/CreateOrder/CreateOrderParams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCasesDTOs.CreateOrder
+{
+    public class CreateOrderParams
+    {
+        public String CustomerId { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipCountry { get; set; }
+        public string ShipPostalCode { get; set;}
+        public List<CreateOrderDetailParams> OrdeDetails { get; set; }
+    }
+}
