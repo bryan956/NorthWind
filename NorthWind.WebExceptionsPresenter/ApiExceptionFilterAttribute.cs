7ccad86e6cdebc0cb8c4ccc71f41fb79ad84e093
estudiantes <estudiantes@CLOCC5PC4769>
2024-03-21 18:30:59 -0500

    Presenters

---
+++ NorthWind.WebExceptionsPresenter/ApiExceptionFilterAttribute.cs
@@ -0,0 +1,33 @@
+﻿using MediatR.Pipeline;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.WebExceptionsPresenter
+{
+    public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        readonly IDictionary<Type, IExcptionHandler> ExceptionHandlers;
+
+        public ApiExceptionFilterAttribute(
+            IDictionary<Type, IExcptionHandler> exceptionHandler) => ExceptionHandlers = exceptionHandler;
+
+        public override void OnException(ExceptionContext context)
+        {
+            Type ExceptionsType = context.Exception.GetType();
+            if(ExceptionHandlers.ContainsKey(ExceptionsType))
+            {
+                ExceptionHandlers[ExceptionsType].Handler(context);
+            }
+            else
+            {
+                new ExceptionHandlerBase().SetResult(context,StatusCodes.Status500InternalServerError, "Ha ocurrido un proceso al procesar la respuesta.", "")
+            }
+            base.OnException(context);
+        }
+    }
+}
---
+++ NorthWind.WebExceptionsPresenter/ValidationExceptionHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NorthWind.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.WebExceptionsPresenter
+{
+    public class ValidationExceptionHandler : ExceptionHandlerBase, IExcptionHandler
+    {
+        public Task Handler(ExceptionContext context)
+        {
+            var Exception = context.Exception as ValidationException;
+            StringBuilder Builder= new StringBuilder();
+            foreach (var Failture in Exception.Errors)
+            {
+                Builder.Append(
+                    string.Format("Propiedad: {0}. Error: {1}",
+                    Failture.PropertyName, Failture.ErrorMessage));
+            }
+            return SetResult(context, StatusCodes.Status400BadRequest, "Error en los datos de entrada", Builder.ToString());
+        }
+    }
+}
